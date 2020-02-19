33ae5b1019f0bdb268564238e275f7d5a74aceef
VirAlex <alexou73@gmail.com>
2020-02-19 15:15:01 +0100

    recipes place to roles

--- src/components/Recipes.vue
+++ src/components/Recipes.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="recipes">
     <p>test</p>
-    <ul v-if="roles">
+    <ul v-if="recipes">
       <li v-for="recipe of recipes" v-bind:key="recipe.idMeal">
         {{ recipe.strMeal }}
       </li>
