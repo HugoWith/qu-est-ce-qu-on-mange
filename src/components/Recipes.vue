e6996f019ba4e734aead17748884f05b30e10360
Hugo Valla <Hugo_valla@hotmail.fr>
2020-02-20 11:17:45 +0100

    ingredient en liste + mesure

--- src/components/Recipes.vue
+++ src/components/Recipes.vue
@@ -10,7 +10,7 @@
     </div>
     <div class="ingredients-list">
       <ul>
-        <li v-for="(ingredient, index) in ingredients" :key="index">{{ingredient.ingredient}}</li>
+        <li v-for="(ingredient, index) in ingredients" :key="index">{{ ingredient }}</li>
       </ul>
     </div>
   </div>
@@ -40,15 +40,53 @@ export default {
           this.recipes = response.data.meals;
           this.ingredients.push(
             response.data.meals[0].strIngredient1 +
-              response.data.meals[0].strIngredient2 +
-              response.data.meals[0].strIngredient3 +
-              response.data.meals[0].strIngredient4 +
-              response.data.meals[0].strIngredient5 +
-              response.data.meals[0].strIngredient6 +
-              response.data.meals[0].strIngredient7 +
-              response.data.meals[0].strIngredient8 +
-              response.data.meals[0].strIngredient9 +
-              response.data.meals[0].strIngredient10
+              " - " +
+              response.data.meals[0].strMeasure1
+          );
+          this.ingredients.push(
+            response.data.meals[0].strIngredient2 +
+              " - " +
+              response.data.meals[0].strMeasure2
+          );
+          this.ingredients.push(
+            response.data.meals[0].strIngredient3 +
+              " - " +
+              response.data.meals[0].strMeasure3
+          );
+          this.ingredients.push(
+            response.data.meals[0].strIngredient4 +
+              " - " +
+              response.data.meals[0].strMeasure4
+          );
+          this.ingredients.push(
+            response.data.meals[0].strIngredient5 +
+              " - " +
+              response.data.meals[0].strMeasure5
+          );
+          this.ingredients.push(
+            response.data.meals[0].strIngredient6 +
+              " - " +
+              response.data.meals[0].strMeasure6
+          );
+          this.ingredients.push(
+            response.data.meals[0].strIngredient7 +
+              " - " +
+              response.data.meals[0].strMeasure7
+          );
+          this.ingredients.push(
+            response.data.meals[0].strIngredient8 +
+              " - " +
+              response.data.meals[0].strMeasure8
+          );
+          this.ingredients.push(
+            response.data.meals[0].strIngredient9 +
+              " - " +
+              response.data.meals[0].strMeasure9
+          );
+          this.ingredients.push(
+            response.data.meals[0].strIngredient10 +
+              " - " +
+              response.data.meals[0].strMeasure10
           );
           // const html = `<li>${this.ingredients}</li>`;
           // document
