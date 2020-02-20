751a3a48854e099912aff941e54c205aaefb4086
Hugo Valla <Hugo_valla@hotmail.fr>
2020-02-20 09:40:52 +0100

    list ingredient

--- src/components/Recipes.vue
+++ src/components/Recipes.vue
@@ -1,37 +1,43 @@
 <template>
-          <div>
-            <div v-for="(recipe, index) in recipes" v-bind:key="index">
-              <div>
-                <h1>Coucou</h1>
-                <h3>{{ recipe.strMeal }}</h3>
-              </div>
-          </div>
-        </div>
+  <div>
+    <div v-for="(recipe, index) in recipes" v-bind:key="index">
+      <div>
+        <h1>Coucou</h1>
+        <h3>{{ recipe.strMeal }}</h3>
+        <ul class="ingredient"></ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
-  name: 'Recipes',
-  data () {
+  name: "Recipes",
+  data() {
     return {
       recipes: [],
       loading: false
-    }
+    };
   },
   methods: {
-    getRecipes: function () {
+    getRecipes: function() {
       this.loading = true;
-      axios.get("https://www.themealdb.com/api/json/v1/1/random.php")
-      .then((response)  =>  {
-        console.log(response);
-        this.loading = false;
-        this.recipes = response.data.meals
-        })
+      axios
+        .get("https://www.themealdb.com/api/json/v1/1/random.php")
+        .then(response => {
+          console.log(response);
+          this.loading = false;
+          this.recipes = response.data.meals;
+          this.recipe = `<li>${recipe.strIngredient1}</li>`;
+          document
+            .querySelector(".ingredients")
+            .insertAdjacentHTML("beforeend", this.recipe);
+        });
     }
   },
-   beforeMount(){
-    this.getRecipes()
- },
-}
+  beforeMount() {
+    this.getRecipes();
+  }
+};
 </script>
